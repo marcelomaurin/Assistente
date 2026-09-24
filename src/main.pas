5ca91399c81ab74246a153d14d85355bff183e45
Marcelo Maurin <marcelomaurin@gmail.com>
2026-09-24 11:23:48 -0300

    feat(voice): implement continuous listening with VAD, self-speech echo suppression, and barge-in support

--- src/main.pas
+++ src/main.pas
@@ -7,7 +7,7 @@ interface
 uses
   Classes, SysUtils, fpjson, jsonparser, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls, LCLType,
   Buttons, ComCtrls, Menus, strutils, chatgpt, setmain, frmconfig,
-  aivoiceprovider_types, aivoicesynthesizer, aivoicerecognizer, aiaudio, aiaudioplayback, aiavatartypes, aiavatar3d, aiinteractioncontext, aiconversationorchestrator, aipersonsession, aipresentation, aikinect_types, aikinectsensor, aikinectskeleton, aikinectperception, aikinectadapter, jarvis_api, agent_manager, project_manager;
+  aivoiceprovider_types, aivoicesynthesizer, aivoicerecognizer, aicontinuouslistener, aiaudio, aiaudioplayback, aiavatartypes, aiavatar3d, aiinteractioncontext, aiconversationorchestrator, aipersonsession, aipresentation, aikinect_types, aikinectsensor, aikinectskeleton, aikinectperception, aikinectadapter, jarvis_api, agent_manager, project_manager;
 
 type
   TPublicViewMode = (
@@ -190,6 +190,8 @@ type
   private
     FAguardandoResposta: Boolean;
     FVoiceActive: Boolean;
+    FAssistantSpeaking: Boolean;
+    FContinuousListener: TAIContinuousListener;
     FAudioInput: TAIAudioInput;
     FAudioPlayer: TAIAudioPlayer;
     FListeningWavFile: string;
@@ -250,6 +252,15 @@ type
     procedure OnAgentComplete(Sender: TObject; const AResponseText, AProvider: string; ASuccess: Boolean);
     procedure AplicaConfiguracoes();
     procedure VoiceRecognized(Sender: TObject; const AText: string);
+    procedure ProcessUserSpeech(const AText: string);
+    function ValidateWavFile(const AFileName: string; out AError: string): Boolean;
+    procedure OnContinuousSpeechUtterance(Sender: TObject; const AWavFileName: string; ADurationMs: Integer);
+    procedure OnContinuousStateChanged(Sender: TObject; AOldState, ANewState: TAIListeningState);
+    procedure OnContinuousRMSChanged(Sender: TObject; ARMS: Double);
+    procedure OnContinuousAudioOrigin(Sender: TObject; AOrigin: TAudioOrigin; AConfidence: Double);
+    procedure OnContinuousBargeIn(Sender: TObject);
+    procedure OnAudioPlayerPlaybackStart(Sender: TObject; const AFileName: string);
+    procedure OnAudioPlayerPlaybackStop(Sender: TObject);
     procedure AtualizaEstadoSpeaker();
     procedure CarregaIcones();
   public
@@ -261,6 +272,7 @@ type
     property VoiceRecog: TAIVoiceRecognizer read FVoiceRecog;
     property AudioInput: TAIAudioInput read FAudioInput;
     property AudioPlayer: TAIAudioPlayer read FAudioPlayer;
+    property ContinuousListener: TAIContinuousListener read FContinuousListener;
     property VoiceActive: Boolean read FVoiceActive;
     property AssistantManager: TAssistantManager read FAssistantManager;
   end;
@@ -1069,6 +1081,10 @@ end;
 
 procedure Tfrmmain.OnVoiceSpeechStart(Sender: TObject);
 begin
+  FAssistantSpeaking := True;
+  if FContinuousListener <> nil then
+    FContinuousListener.NotifyAssistantSpeechStart;
+
   if FAvatar3D <> nil then
     FAvatar3D.SetState(avSpeaking);
   if FPublicViewMode = pvmPresentation then
@@ -1079,12 +1095,18 @@ end;
 
 procedure Tfrmmain.OnVoiceSpeechEnd(Sender: TObject);
 begin
+  FAssistantSpeaking := False;
+  if FContinuousListener <> nil then
+    FContinuousListener.NotifyAssistantSpeechEnd;
+
   if FAvatar3D <> nil then
     FAvatar3D.SetState(avIdle);
   if FPublicViewMode = pvmPresentation then
     SetProfessorState('presenting')
   else if FPublicViewMode = pvmContent then
     SetProfessorState('showing_content')
+  else if (FContinuousListener <> nil) and FContinuousListener.Enabled then
+    SetProfessorState('listening')
   else
     SetProfessorState('idle');
 end;
@@ -1201,6 +1223,21 @@ begin
   FVoiceRecog := TAIVoiceRecognizer.Create(Self);
   FVoiceRecog.OnRecognized := @VoiceRecognized;
 
+  // Inicializa escuta contínua com VAD e supressão da própria voz
+  FContinuousListener := TAIContinuousListener.Create(Self);
+  FContinuousListener.Continuous := True;
+  FContinuousListener.OnSpeechUtterance := @OnContinuousSpeechUtterance;
+  FContinuousListener.OnStateChanged := @OnContinuousStateChanged;
+  FContinuousListener.OnRMSChanged := @OnContinuousRMSChanged;
+  FContinuousListener.OnAudioOriginDetected := @OnContinuousAudioOrigin;
+  FContinuousListener.OnBargeIn := @OnContinuousBargeIn;
+
+  if Assigned(FAudioPlayer) then
+  begin
+    FAudioPlayer.OnPlaybackStart := @OnAudioPlayerPlaybackStart;
+    FAudioPlayer.OnPlaybackStop := @OnAudioPlayerPlaybackStop;
+  end;
+
   CHATGPT1 := TCHATGPT.Create(Self);
 
   FProjectManager := TAssistantProjectManager.Create(Self);
@@ -1280,6 +1317,8 @@ end;
 
 procedure Tfrmmain.FormClose(Sender: TObject; var CloseAction: TCloseAction);
 begin
+  if Assigned(FContinuousListener) then
+    FContinuousListener.StopListening;
   ShutdownKinect;
   if (FSetMain <> nil) and FSetMain.MinimizeToTray and (CloseAction <> caFree) then
   begin
@@ -1365,7 +1404,30 @@ begin
       FVoiceRecog.Language := FSetMain.RecogLanguage
     else
       FVoiceRecog.Language := 'pt';
-    FVoiceRecog.OpenAIToken := FSetMain.CHATGPT;
+
+    if (FSetMain <> nil) and (Trim(FSetMain.STTToken) <> '') then
+      FVoiceRecog.OpenAIToken := FSetMain.STTToken
+    else
+      FVoiceRecog.OpenAIToken := FSetMain.CHATGPT;
+
+    if (FSetMain <> nil) and (Trim(FSetMain.STTModel) <> '') then
+      FVoiceRecog.OpenAIModel := FSetMain.STTModel;
+    if (FSetMain <> nil) and (Trim(FSetMain.STTEndpoint) <> '') then
+      FVoiceRecog.OpenAIEndpoint := FSetMain.STTEndpoint;
+  end;
+
+  if Assigned(FContinuousListener) and (FSetMain <> nil) then
+  begin
+    FContinuousListener.Enabled := FSetMain.ContinuousListening;
+    FContinuousListener.VoiceThreshold := FSetMain.VoiceThreshold;
+    FContinuousListener.SilenceTimeoutMs := FSetMain.SilenceTimeoutMs;
+    FContinuousListener.MinSpeechMs := FSetMain.MinSpeechMs;
+    FContinuousListener.MaxSpeechMs := FSetMain.MaxSpeechMs;
+    FContinuousListener.EchoSuppressionEnabled := FSetMain.EchoSuppressionEnabled;
+    FContinuousListener.SelfAudioCorrelationThreshold := FSetMain.SelfAudioCorrelationThreshold;
+
+    if FContinuousListener.Enabled then
+      FContinuousListener.StartListening;
   end;
 
   // Configurações do Sintetizador de Voz (TAIVoiceSynthesizer do CHATGPT)
@@ -1572,10 +1634,10 @@ begin
     if FileExists(FListeningWavFile) then
     begin
       AdicionaMensagemHistorico('Voz', 'Transcrevendo áudio gravado via TAIVoiceRecognizer...');
-      if (FVoiceRecog <> nil) and FVoiceRecog.Recognize(FListeningWavFile) then
+      if FVoiceRecog <> nil then
       begin
-        AdicionaMensagemHistorico('Você (Voz)', FVoiceRecog.RecognizedText);
-        ExecutaComandoJarvis(FVoiceRecog.RecognizedText);
+        // Recognize já dispara OnRecognized -> VoiceRecognized -> ProcessUserSpeech
+        FVoiceRecog.Recognize(FListeningWavFile);
       end
       else if FVoiceRecog <> nil then
       begin
@@ -1590,16 +1652,213 @@ begin
 end;
 
 procedure Tfrmmain.VoiceRecognized(Sender: TObject; const AText: string);
-var
-  Info: string;
 begin
-  Info := Trim(AText);
-  if Info <> '' then
+  ProcessUserSpeech(AText);
+end;
+function Tfrmmain.ValidateWavFile(const AFileName: string; out AError: string): Boolean;
+var
+  FS: TFileStream;
+  RiffTag, WaveTag: array[0..3] of AnsiChar;
+begin
+  Result := False;
+  AError := '';
+  if not FileExists(AFileName) then
   begin
-    AdicionaMensagemHistorico('Você (Voz)', Info);
-    ExecutaComandoJarvis(Info);
+    AError := 'Arquivo de áudio não encontrado';
+    Exit;
+  end;
+
+  try
+    FS := TFileStream.Create(AFileName, fmOpenRead or fmShareDenyNone);
+    try
+      if FS.Size <= 44 then
+      begin
+        AError := 'Tamanho do arquivo insuficiente';
+        Exit;
+      end;
+
+      FS.ReadBuffer(RiffTag, 4);
+      FS.Position := 8;
+      FS.ReadBuffer(WaveTag, 4);
+      if (RiffTag <> 'RIFF') or (WaveTag <> 'WAVE') then
+      begin
+        AError := 'Formato WAV inválido';
+        Exit;
+      end;
+
+      Result := True;
+    finally
+      FS.Free;
+    end;
+  except
+    on E: Exception do
+      AError := E.Message;
   end;
 end;
+
+procedure Tfrmmain.ProcessUserSpeech(const AText: string);
+var
+  TextoLimpo: string;
+begin
+  TextoLimpo := Trim(AText);
+  if TextoLimpo = '' then Exit;
+
+  AdicionaMensagemHistorico('Você (Voz)', TextoLimpo);
+
+  // Notifica orquestrador de conversação e contexto
+  if FConversationOrchestrator <> nil then
+  begin
+    FConversationOrchestrator.NotifySpeechStart;
+    FConversationOrchestrator.Context.ResolveReference(TextoLimpo);
+    if (FAssistantManager <> nil) and (FConversationOrchestrator.Context.CurrentProject <> '') then
+      FAssistantManager.ActiveProject := FConversationOrchestrator.Context.CurrentProject;
+    if FConversationOrchestrator.SessionManager.ActiveSession <> nil then
+      FConversationOrchestrator.SessionManager.ActiveSession.AddMessage('user', TextoLimpo);
+  end;
+
+  if FAvatar3D <> nil then
+  begin
+    FAvatar3D.SetState(avListening);
+    FAvatar3D.SetState(avThinking);
+  end;
+
+  SetProfessorState('thinking');
+
+  // Se o Professor Virtual estiver em apresentação guiada, responde com desvio didático
+  if (FPresentationAgent <> nil) and (FPresentationAgent.State in [psPresentingConcept, psGreeting]) then
+  begin
+    FPresentationAgent.AnswerQuestion(TextoLimpo);
+    Exit;
+  end;
+
+  // Pipeline Inteligente do Agente
+  FAguardandoResposta := True;
+  btEnviar.Enabled := False;
+
+  if Assigned(FAssistantManager) then
+    FAssistantManager.ProcessUserRequestAsync(TextoLimpo)
+  else if Trim(FSetMain.JarvisURL) <> '' then
+    TAskJarvisThread.Create(TextoLimpo, FSetMain.JarvisIAMode, FSetMain.JarvisURL, FSetMain.JarvisAPIKey)
+  else
+    TAskChatGPTThread.Create(TextoLimpo);
+end;
+
+procedure Tfrmmain.OnContinuousSpeechUtterance(Sender: TObject; const AWavFileName: string; ADurationMs: Integer);
+var
+  ValErr: string;
+begin
+  if not FileExists(AWavFileName) then Exit;
+
+  if not ValidateWavFile(AWavFileName, ValErr) then
+  begin
+    AdicionaMensagemHistorico('Voz', 'Áudio descartado: ' + ValErr);
+    try DeleteFile(AWavFileName); except end;
+    Exit;
+  end;
+
+  SetProfessorState('thinking');
+  AdicionaMensagemHistorico('Voz', Format('🎤 Fala capturada (%.1fs). Reconhecendo...', [ADurationMs / 1000.0]));
+
+  if FVoiceRecog <> nil then
+    FVoiceRecog.Recognize(AWavFileName);
+
+  try
+    DeleteFile(AWavFileName);
+  except
+  end;
+end;
+
+procedure Tfrmmain.OnContinuousStateChanged(Sender: TObject; AOldState, ANewState: TAIListeningState);
+begin
+  case ANewState of
+    lsListening:
+      if not FAssistantSpeaking then
+        SetProfessorState('listening');
+    lsSpeechDetected, lsCapturingSpeech:
+      begin
+        if Assigned(lblProfessorStatus) then
+        begin
+          lblProfessorStatus.Font.Color := $0000D4FF;
+          lblProfessorStatus.Caption := '🎤 Você está falando...';
+        end;
+        if FAvatar3D <> nil then
+          FAvatar3D.SetState(avListening);
+      end;
+    lsProcessing:
+      SetProfessorState('thinking');
+    lsSuppressedByAssistantSpeech:
+      ; // Supressão silenciosa da própria voz
+  end;
+end;
+
+procedure Tfrmmain.OnContinuousRMSChanged(Sender: TObject; ARMS: Double);
+var
+  Bars, TotalBars, i: Integer;
+  BarStr: string;
+begin
+  if Assigned(lblStatVoice) and (FContinuousListener <> nil) then
+  begin
+    TotalBars := 10;
+    Bars := Round(ARMS * 200.0);
+    if Bars < 0 then Bars := 0;
+    if Bars > TotalBars then Bars := TotalBars;
+    BarStr := '[';
+    for i := 1 to Bars do BarStr := BarStr + '█';
+    for i := Bars + 1 to TotalBars do BarStr := BarStr + '░';
+    BarStr := BarStr + ']';
+
+    if FContinuousListener.State = lsSuppressedByAssistantSpeech then
+      lblStatVoice.Caption := Format('● Áudio: [Supressão TTS ativa] RMS: %.3f %s', [ARMS, BarStr])
+    else if FContinuousListener.State in [lsSpeechDetected, lsCapturingSpeech] then
+      lblStatVoice.Caption := Format('● Áudio: [Voz Detectada] RMS: %.3f %s', [ARMS, BarStr])
+    else if FContinuousListener.State = lsListening then
+      lblStatVoice.Caption := Format('● Áudio: [Ouvindo] RMS: %.3f %s', [ARMS, BarStr]);
+  end;
+end;
+
+procedure Tfrmmain.OnContinuousAudioOrigin(Sender: TObject; AOrigin: TAudioOrigin; AConfidence: Double);
+begin
+  // Monitoramento de origem
+end;
+
+procedure Tfrmmain.OnContinuousBargeIn(Sender: TObject);
+begin
+  AdicionaMensagemHistorico('Voz', '⚡ Interrupção do visitante detectada (Barge-In)!');
+
+  if FVoiceSynth <> nil then
+    FVoiceSynth.Stop;
+  if (FAudioPlayer <> nil) and FAudioPlayer.Playing then
+    FAudioPlayer.Stop;
+
+  FAssistantSpeaking := False;
+
+  if FAvatar3D <> nil then
+  begin
+    FAvatar3D.CancelGesture;
+    FAvatar3D.SetState(avListening);
+  end;
+
+  SetProfessorState('listening');
+
+  if FConversationOrchestrator <> nil then
+    FConversationOrchestrator.NotifySpeechStart;
+end;
+
+procedure Tfrmmain.OnAudioPlayerPlaybackStart(Sender: TObject; const AFileName: string);
+begin
+  if FContinuousListener <> nil then
+  begin
+    FContinuousListener.FeedPlaybackWav(AFileName);
+    FContinuousListener.NotifyAssistantSpeechStart;
+  end;
+end;
+
+procedure Tfrmmain.OnAudioPlayerPlaybackStop(Sender: TObject);
+begin
+  if FContinuousListener <> nil then
+    FContinuousListener.NotifyAssistantSpeechEnd;
+end;
+
 
 procedure Tfrmmain.btSpeakerClick(Sender: TObject);
 begin
